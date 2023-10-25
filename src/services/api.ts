5eade20d26322fcbf65259770265d05d88cd29e9
hari6091 <hari.junior19@gmail.com>
2023-10-25 11:05:47 -0300

    feat: update connection

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://localhost:5000",
+  baseURL: "http://172.23.0.3:5000",
   headers: {
     accept: "*/*",
     "Content-Type": "application/json",
