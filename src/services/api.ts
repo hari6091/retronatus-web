1e48f508816e3f9c3f09268614d2f9c9479c4cd5
hari6091 <hari.junior19@gmail.com>
2023-10-25 12:20:21 -0300

    feat: add axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,10 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://backend:5000",
-  headers: {
-    accept: "*/*",
-    "Content-Type": "application/json",
-  },
 });
 
 export default api;
