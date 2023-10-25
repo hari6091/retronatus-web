7cfc21779b772a9450cfd9fe2de6bea3ecfce93e
hari6091 <hari.junior19@gmail.com>
2023-10-25 12:28:03 -0300

    feat: update axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,12 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: 'http://localhost:5000',
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+  },
+  withCredentials: true,
 });
 
 export default api;
